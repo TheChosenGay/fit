f4fe07c18f93c442ab3f48cdab5c223fabd73ccf
rickdai <rickdai@tencent.com>
2026-05-08 23:15:50 +0800

    Initial Commit

---
+++ fit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fit
+//
+//  Created by dai shan on 2026/5/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fit/fitApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitApp.swift
+//  fit
+//
+//  Created by dai shan on 2026/5/8.
+//
+
+import SwiftUI
+
+@main
+struct fitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
